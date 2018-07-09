d062288947cc8a1be37de0848d82a389f2a7299f
Victor RUELLE <victor.ruelle@polytechnique.edu>
2018-07-09 14:00:12 +0200

    Replace globals.py

--- globals.py
+++ globals.py
@@ -1,6 +1,6 @@
 # from numpy.random import random as rd
 eps = 0.1
-debug = False
+debug = True
 max_column_generation_count = 50
 max_depth = 10                  #max number of branching 
 max_time_for_shrinking = 5
@@ -8,12 +8,13 @@ max_size_for_shrinking = 4 #inclusive
 max_global_time = 1000
 vals = [0.01,0.5,0.9]
 
-class modifiable_global():
+class modifiable_threshold():
 	def __init__(self,initial_value):
 		self.value = initial_value
+		self.vals = vals
 		
 	def update(self,):
 		# self.value = 0.9
 		self.value = vals[(vals.index(self.value)+1)%len(vals)]
 		
-connected_threshold = modifiable_global(0.5)
+connected_threshold = modifiable_threshold(vals[0])
