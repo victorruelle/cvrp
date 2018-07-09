6c6e91db70f7963ac6cb407891c14b7a136885ee
Victor RUELLE <victor.ruelle@polytechnique.edu>
2018-07-09 09:55:05 +0200

    Add new file

---
+++ constraint.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 26 13:39:47 2018
+
+@author: GVKD1542
+"""
+
+from cap_constraint import * 
+# from comb_constraint import *
+# from multistar_constraint import *
+# from hypotour_constraint import * 
+
+""" missing :
+    gomory cuts
+    framed capacity cuts
+"""
